5e1283e9ca444587a657ddce0f239c778b1c4ab0
Gordon Burns <gordonburns@live.co.uk>
2023-06-06 16:30:11 +0100

    Prepwork for using API

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for selected days")
 place = st.text_input("Place: ")
 days = st.slider(label="Forecast Days", max_value=5)
-options = st.selectbox(label="Select data to view", options=("Temperature", "Sky"))
+options = st.selectbox(label="Select data to view",
+                       options=("Temperature", "Sky"))
 
 st.subheader(f"{options} for the next {days}  days in {place}")
+
+def get_data(days)
+    dates = ["2022-10-25", "2022-10-26", "2022-10-27"]
+    temperatures = [10, 12, 20]
+    temperatures = [days * i for i in temperatures]
+    return dates,temperatures
+
+d,t = get_data(days)
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
