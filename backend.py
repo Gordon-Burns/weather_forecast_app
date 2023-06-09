15d397903c6dc01986e96f66f830cfc0791a4517
Gordon Burns <gordonburns@live.co.uk>
2023-06-09 14:34:00 +0100

    Created get_data function

--- backend.py
+++ backend.py
@@ -8,8 +8,15 @@ def get_data(place, days, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_of_values = 8 * days
+    filtered_data = filtered_data[:nr_of_values]
+    if kind == "Temperature":
+        filtered_data = [i["main"]["temp"] for i in filtered_data]
+    if kind == "Sky":
+        filtered_data = [i["weather"][0]["main"] for i in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Glasgow", days=5))
+    print(get_data(place="Glasgow", days=5, kind="Sky"))
